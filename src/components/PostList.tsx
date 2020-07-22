893881d187745e9c0ab0cec2f1552fa23e1be8fc
Salomón Piña <sarquamon@gmail.com>
2020-07-22 18:40:08 -0500

    Component is now able to check if content is null

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -3,50 +3,53 @@ import { Link } from "gatsby";
 import { slugify } from "../utils/handlers";
 
 interface props {
-  posts: {
-    id: string;
-    slug: string;
-    tags: string[];
-    date: string;
-    title: string;
-    image: string;
-    category: string;
-  }[];
+  posts:
+    | {
+        id: string;
+        slug: string;
+        tags: string[];
+        date: string;
+        title: string;
+        image: string;
+        category: string;
+      }[]
+    | null;
 }
 
 export default function PostList(data: props) {
   return (
     <div className="grid postListGrid">
-      {data.posts.map(post => {
-        return (
-          <Link className="postList-container" to={post.slug} key={post.id}>
-            {post.image && (
-              <div className="postListImage">
-                <img src={post.image} alt="post image" loading="lazy" />
+      {data.posts &&
+        data.posts.map(post => {
+          return (
+            <Link className="postList-container" to={post.slug} key={post.id}>
+              {post.image && (
+                <div className="postListImage">
+                  <img src={post.image} alt="post image" loading="lazy" />
+                </div>
+              )}
+              <div className="postListData">
+                <time>{post.date}</time>
+                <div>{post.title}</div>
               </div>
-            )}
-            <div className="postListData">
-              <time>{post.date}</time>
-              <div>{post.title}</div>
-            </div>
-            <div className="postTags">
-              {post.tags &&
-                post.tags.map(tag => (
-                  <Link
-                    key={tag}
-                    to={`tags/${slugify(tag)}/`}
-                    className={`tag tag-${tag}`}
-                  >
-                    #{tag}
-                  </Link>
-                ))}
-              <Link to={`/${post.category}/`} className="postListCat">
-                #{post.category}
-              </Link>
-            </div>
-          </Link>
-        );
-      })}
+              <div className="postTags">
+                {post.tags &&
+                  post.tags.map(tag => (
+                    <Link
+                      key={tag}
+                      to={`tags/${slugify(tag)}/`}
+                      className={`tag tag-${tag}`}
+                    >
+                      #{tag}
+                    </Link>
+                  ))}
+                <Link to={`/${post.category}/`} className="postListCat">
+                  #{post.category}
+                </Link>
+              </div>
+            </Link>
+          );
+        })}
     </div>
   );
 }
