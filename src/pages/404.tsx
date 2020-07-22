d68e656dfbb98cb74610206daf11b4d325de2c71
Salomón Piña <sarquamon@gmail.com>
2020-07-22 17:41:54 -0500

    Restarted layout

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,20 +2,15 @@ import React from "react";
 import { Helmet } from "react-helmet";
 
 import Layout from "../components/Layout";
-import config from "../utils/config";
 import SEO from "../components/SEO";
+import config from "../utils/config";
 
 export default function FouroFour() {
   return (
     <Layout>
       <Helmet title={`404 | ${config.siteTitle}`} />
       <SEO />
-      <section className="container">
-        <h1 className="notFoundTitle">404</h1>
-        <p className="notFoundDesc">
-          The page you're looking for was not found.
-        </p>
-      </section>
+      <h1>404</h1>
     </Layout>
   );
 }
