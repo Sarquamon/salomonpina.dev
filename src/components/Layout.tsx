7b91243313577c5adf7c5abbbc4f2ae4c5228040
Salomón Piña <sarquamon@gmail.com>
2020-07-24 03:51:18 -0500

    Updated git case

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react";
+import { Helmet } from "react-helmet";
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+import "../snowy.css";
+import "../style.css";
+
+interface props {
+  children?: ReactNode;
+}
+
+export default function Layout({ children }: props) {
+  return (
+    <div>
+      <Helmet>
+        <link rel="shortcut icon" type="image/png" href="/appLogo.png" />
+      </Helmet>
+      <Navbar />
+      <main className="container">{children}</main>
+      <Footer />
+    </div>
+  );
+}
