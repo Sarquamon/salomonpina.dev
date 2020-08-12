1f42e125ffb50b2958cea7ef3a27e4450a2342a0
Salomón Piña <sarquamon@gmail.com>
2020-08-12 18:06:54 -0500

    Updated & created basic listing for categories

--- src/pages/journeys.tsx
+++ src/pages/journeys.tsx
@@ -1,13 +1,50 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { graphql } from "gatsby";
 
 import SEO from "../components/SEO";
 import Layout from "../components/Layout";
+import Card from "../components/Card";
 
-export default function Journeys() {
+import { optimizedJourneysCategories } from "../utils/handlers";
+
+export default function Journeys({ data }: any) {
+  const optimizedCategories = optimizedJourneysCategories(
+    data.allMarkdownRemark.edges
+  );
+
   return (
     <Layout>
+      <Helmet title="Journeys" />
       <SEO />
       <h1>Journeys</h1>
+      <div>
+        {optimizedCategories &&
+          optimizedCategories.map((category, index) => {
+            return <Card key={index} category={category} />;
+          })}
+      </div>
     </Layout>
   );
 }
+
+export const journeysQuery = graphql`
+  query journeysComponentQuery {
+    allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          category: { in: ["Journeys", "journey", "journeys", "Journey"] }
+        }
+      }
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            tags
+          }
+        }
+      }
+    }
+  }
+`;
