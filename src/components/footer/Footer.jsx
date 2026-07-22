ab6dfd78f897410d3594c9f0bcc281038266b609
Salomón Piña <sarquamon@gmail.com>
2026-07-21 22:19:27 -0600

    Update footer syles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,22 +6,61 @@ import "./footer.css";
 export const Footer = () => {
   return (
     <footer className="footer_footer">
-      <nav className="footer_content">
-        <ul className="navbar_list">
-          <li className={"navbar_item"}>
-            <NavLink to={"/"}>Home</NavLink>
-          </li>
-          <li className={"navbar_item"}>
-            <NavLink to={"/about"}>About</NavLink>
-          </li>
-          <li className={"navbar_item"}>
-            <NavLink to={"/tutorials"}>Tutorials</NavLink>
-          </li>
-          <li className={"navbar_item"}>
-            <NavLink to={"/sitemap"}>Sitemap</NavLink>
-          </li>
-        </ul>
-      </nav>
+      <div className="footer_content">
+        <div>
+          {/* TODO: Update wording */}
+          <NavLink to={"/"}>
+            <img src="" alt="logo" />
+          </NavLink>
+          <p>
+            Hey! I'm Salomón Piña Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Molestiae, rerum.
+          </p>
+          <br />
+          <p>salomonpina.dev licenses all source code on this website under the MIT License.</p>
+        </div>
+        <nav className="footer_nav_content">
+          <ul className="footer_navbar_list">
+            <li>
+              <h2>Links</h2>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/"}>Home</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/about"}>About</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/tutorials"}>Tutorials</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/sitemap"}>Sitemap</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <nav className="footer_nav_content">
+          <ul className="footer_navbar_list">
+            <li>
+              <h2>About</h2>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/aboutme"}>About Me</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/license"}>License</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/privacypolicy"}>Privacy Policy</NavLink>
+            </li>
+            <li className={"footer_navbar_item"}>
+              <NavLink to={"/contact"}>Contact Me</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="copyright_wrapper">
+        <p className="copyright_statement">© 2026 Sarquamon - Built with love.</p>
+      </div>
     </footer>
   );
 };
